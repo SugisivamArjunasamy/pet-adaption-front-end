c24670b7d29a40d9f31b9a7d899601f49bfe9348
Varun Prashanth V <v.varunprashanth@gmail.com>
2025-03-25 15:28:46 +0530

    ignore

--- React-Pet-Adaption/Pet-Adaption/src/App.jsx
+++ React-Pet-Adaption/Pet-Adaption/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // ✅ Fixed BrowserRouter typo
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
 import About from "./components/about";
 import Home from "./components/home.jsx";
 import Footer from "./components/footer";
